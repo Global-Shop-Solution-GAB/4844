d4c5783858ab8f00ab82e30cd56b11fcbf40935e
Service Web <sw@gssmail.com>
2024-03-12 17:22:57 -0500

    Commit changes in C:\Websites\Git\emarlow\4844\GCG_4844_TrackHist.sql

---
+++ GCG_4844_TrackHist.sql
@@ -0,0 +1,8 @@
+/* ==========================================
+ * TABLE: GCG_4844_TrackHist
+ * ========================================== */
+CREATE TABLE "GCG_4844_TrackHist"(
+ "PO_Number" CHAR(50),
+ "Tracking" CHAR(50));
+
+
